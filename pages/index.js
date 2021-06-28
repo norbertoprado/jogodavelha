bea08c77b530f44f7397f99402d3a29d085de541
Norberto Silva Prado <nsprado@yahoo.com.br>
2021-06-28 00:26:58 -0300

    Segunda mudança

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import {useState} from 'react';
+import Link from 'next/link';
 
 function Home(){
     return (
         <div>
             <h1>Home</h1>
+            <Link href="/sobre">
+                <a>Acessar página Sobre</a>
+            </Link>
             <Contador />
-
         </div>)
 }
 
